3fc270ffc30458a034543ba207355e2b851b1d0f
Jorge Henriquez <joahenri@ucsc.edu>
2019-03-06 01:03:19 -0800

    add printining capabilities

--- length.go
+++ length.go
@@ -1,5 +1,7 @@
 package length
 
+import "fmt"
+
 // A Distance represents a physical distance
 // as an float64 nanometer count. The representation limits the
 // largest representable duration to approximately
@@ -29,3 +31,57 @@ const (
 	Mile                = 5280 * Feet
 	Lightyear           = 9.461e12 * Kilometer
 )
+
+var useMetric = true
+
+// ToggleUnits toggles the units (metric <=> imperial) that are printed whenever the String
+// function is called (as is the case in family of printing functions in the fmt package).
+// By default the metric system is used.
+func ToggleUnits() {
+	useMetric = !useMetric
+}
+
+// String returns a string representing the distance in the form "10m" or "10yd".
+// The unit that is used is based on the state of the ToggleUnit function.
+// As a special case, durations less than one
+// meter (or yard) use a smaller unit to ensure
+// that the leading digit is non-zero. The zero duration formats as 0m or 0yd.
+func (d Distance) String() string {
+	// If in metric mode
+	if useMetric {
+		return d.printMetric()
+	}
+	return d.printImperial()
+}
+
+func (d Distance) printMetric() string {
+	if d >= 1*Meter {
+		return fmt.Sprintf("%fm", float64(d)/float64(Meter))
+	}
+	if d >= 1*Centimeter {
+		return fmt.Sprintf("%fcm", float64(d)/float64(Centimeter))
+	}
+	if d >= 1*Millimeter {
+		return fmt.Sprintf("%fcm", float64(d)/float64(Millimeter))
+	}
+	if d >= 1*Micrometer {
+		return fmt.Sprintf("%fµm", float64(d)/float64(Micrometer))
+	}
+	if d == 0 {
+		return fmt.Sprintf("0m")
+	}
+	return fmt.Sprintf("%fnm", float64(d)/float64(Nanometer))
+}
+
+func (d Distance) printImperial() string {
+	if d >= 1*Yard {
+		return fmt.Sprintf("%fyd", float64(d)/float64(Yard))
+	}
+	if d >= 1*Feet {
+		return fmt.Sprintf("%fft", float64(d)/float64(Feet))
+	}
+	if d == 0 {
+		return fmt.Sprintf("0yd")
+	}
+	return fmt.Sprintf("%fin", float64(d)/float64(Inch))
+}
